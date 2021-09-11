81c9ddc2ed16e9d4019ccdaefe92d6e6eeb40829
Antonio Moreno <ammz@deversorius.net>
2021-09-11 20:12:09 +0200

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+
+/*
+  Author: Antonio Moreno
+  Date: 2021/09/11
+
+  A simple program that show my name
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  printf("Antonio Moreno\n");
+  return 0;
+}
